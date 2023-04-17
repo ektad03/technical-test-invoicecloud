6ade1dd8bcea0547ce96ee17d4735f5e24ca7e52
ekta_deshmukh <ekta.deshmukh01@gmail.com>
2023-04-17 16:24:52 -0700

    Added automation test case to validate n number of elements on web page.

---
+++ AddRemoveElementsTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace InvoiceCloudAssessment
+{
+    /// <summary>
+    ///  This test case adds 'n' number of elements and asserts that 'n' number of elements exist on the page.
+    /// </summary>
+    [TestClass]
+    public class AddRemoveElementsTest
+    {
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            // Set up the ChromeDriver instance
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestMethod]
+        public void AddNElementsAndAssertCount()
+        {
+            // Navigate to the add/remove elements page
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/add_remove_elements/");
+
+            // Add n number of elements
+            int n = 5; // Replace with desired number of elements
+            for (int i = 0; i < n; i++)
+            {
+                driver.FindElement(By.XPath("//button[text()='Add Element']")).Click();
+            }
+
+            // Assert that n number of elements exist on the page
+            int actualCount = driver.FindElements(By.XPath("//button[text()='Delete']")).Count;
+            Assert.AreEqual(n, actualCount, $"Expected {n} elements, but found {actualCount} elements.");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            // Clean up the ChromeDriver instance
+            driver.Quit();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InvoiceCloudAssessment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InvoiceCloudAssessment")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ff833e2c-6bb3-401a-b86a-10ad94a25e2b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
